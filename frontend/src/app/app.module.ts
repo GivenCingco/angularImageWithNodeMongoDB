9cf639bd2089c62dcac1a90643859e19d40b29e6
GivenCingco <givencingco@gmail.com>
2023-06-06 17:40:36 +0200

    Initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserProfileComponent,
+    ProfilesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
